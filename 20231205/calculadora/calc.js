1769091a0ed416716dbce47127ce225d7ff31df5
aGomez9907 <agomez_@live.com.ar>
2023-12-05 22:14:11 -0300

    first working calculator

--- 20231205/calculadora/calc.js
+++ 20231205/calculadora/calc.js
@@ -1,31 +1,85 @@
-// let nombreH1 = document.getElementById("nombre_h1");
-// let apellidoH2 = document.getElementById("apellido_h2");
-// let obtenerTexto = document.getElementById("obtener-texto");
-
-// obtenerTexto.addEventListener("click", function () {
-//   let nombre_input = document.getElementById("nombre").value;
-//   let apellido_input = document.getElementById("apellido").value;
-
-//   apellidoH2.style.color = "red";
-//   nombreH1.textContent = nombre_input;
-//   apellidoH2.textContent = apellido_input;
-// });
-
-let botones = document.getElementsByTagName("button");
+let numericos = document.getElementsByClassName("numerico");
+let operadores = document.getElementsByClassName("operador");
 let display = document.getElementById("display");
+let borrar = document.getElementById("btn_borrar");
+let igual = document.getElementById("btn_igual");
 
-for (let btn of botones) {
+let inputActual = "";
+let operador = "";
+let resultado = 0;
+
+//listeners
+for (let btn of numericos) {
   btn.addEventListener("click", function (e) {
-    escribir(btn.value);
+    agregarEnPantalla(e.target.value);
   });
 }
 
-function escribir(value) {
-  display.innerText += value;
+for (let btn of operadores) {
+  btn.addEventListener("click", function (e) {
+    operadorActual(e.target.value);
+    console.log(inputActual, operador, resultado);
+  });
 }
 
-function calcular() {}
+borrar.addEventListener("click", function () {
+  borrarDisplay();
+});
+
+igual.addEventListener("click", function () {
+  calcular();
+});
+
+//funciones
+function actualizarDisplay() {
+  display.value = inputActual;
+}
+
+function agregarEnPantalla(value) {
+  inputActual += value;
+  actualizarDisplay();
+}
+
+function operadorActual(op) {
+  operador = op;
+  resultado = Number(inputActual);
+  inputActual = "";
+}
+
+function calcular() {
+  switch (operador) {
+    case "+":
+      resultado += Number(inputActual);
+      break;
+    case "-":
+      resultado -= Number(inputActual);
+      break;
+    case "*":
+      resultado *= Number(inputActual);
+      break;
+    case "/":
+      if (inputActual != 0) {
+        resultado /= Number(inputActual);
+      } else {
+        borrarDisplay();
+        display.value = "Error";
+        inputActual = "";
+        resultado = 0;
+        operador = "";
+        return;
+      }
+      break;
+  }
+
+  inputActual = resultado.toString();
+  operador = "";
+  actualizarDisplay();
+}
 
 function borrarDisplay() {
-  display.innerText = "";
+  inputActual = "";
+  operador = "";
+  resultado = 0;
+
+  actualizarDisplay();
 }
